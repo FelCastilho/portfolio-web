cefde8b59e9e7e73b39a135362f7b6ca918f4c69
felipeCast <castfelipe10@gmail.com>
2024-06-28 22:44:10 -0300

    finalizando tecnologias

--- src/home/pages/technology/index.tsx
+++ src/home/pages/technology/index.tsx
@@ -4,12 +4,12 @@ import './style.css'
 import { FaReact, FaSass, FaFigma } from "react-icons/fa";
 import { SiExpo } from "react-icons/si";
 import { TbBrandTypescript } from "react-icons/tb";
-import { TbBrandCss3, TbBrandHtml5, TbBrandJavascript  } from "react-icons/tb";
+import { TbBrandCss3, TbBrandHtml5, TbBrandJavascript } from "react-icons/tb";
 import { LiaGitAlt } from "react-icons/lia";
 
 export function Technology() {
 
-    const [ hover, setHover] = useState('')
+    const [hover, setHover] = useState('')
 
     return (
         <section className="my-techs" id="techs">
@@ -23,65 +23,56 @@ export function Technology() {
                     </div>
 
                     <div className="container-hover">{hover}</div>
-        
+
                 </div>
 
                 <div className="container-icons">
 
                     <div className="first-group">
-                        <FaReact 
-                            className='techs-icon'
-                            onMouseEnter={() => setHover('Desenvolvimento em React Native e  para agilizar processo de criação e desenvolvimento de sites e aplicativos híbridos.')}
-                            onMouseLeave={() => setHover('')}
-                        />
-                        <SiExpo
-                            className='techs-icon'
-                            onMouseEnter={() => setHover('Desenvolvimento de aplicativos com Expo utilizando React native.')}
-                            onMouseLeave={() => setHover('')}
-                        />
-                        <TbBrandTypescript 
-                            className='techs-icon'
-                            onMouseEnter={() => setHover('Aplicações feitas em Typescript para organização do código.')}
-                            onMouseLeave={() => setHover('')}
-                        />
+
                     </div>
-        
+
                     <div className="second-group">
-                        <TbBrandHtml5 
+                        <FaReact
+                            className='techs-icon'
+                            onMouseEnter={() => setHover('Desenvolvimento em React Native e  para agilizar processo de criação e desenvolvimento de sites.')}
+                            onMouseLeave={() => setHover('')}
+                        />
+                        <TbBrandHtml5
                             className='techs-icon'
                             onMouseEnter={() => setHover('Estruturas feita em HTML5 seguindo estruturas semânticas.')}
                             onMouseLeave={() => setHover('')}
                         />
-                        <TbBrandCss3 
+                        <TbBrandCss3
                             className='techs-icon'
                             onMouseEnter={() => setHover('Estilizações feitas em CSS abordando conceitos como Mobile First e responsividade.')}
                             onMouseLeave={() => setHover('')}
                         />
-                        <TbBrandJavascript 
+                        <TbBrandJavascript
                             className='techs-icon'
                             onMouseEnter={() => setHover('Estrutura dinâmica dos sites desenvolvida em Javascript para melhor otimização.')}
                             onMouseLeave={() => setHover('')}
                         />
                     </div>
-        
+
                     <div className="third-group">
-                        <FaSass 
+                        <FaSass
                             className='techs-icon'
                             onMouseEnter={() => setHover('Estilização de sites utilizando Sass para agilizar e organizar o projeto.')}
                             onMouseLeave={() => setHover('')}
                         />
-                        <FaFigma 
+                        <FaFigma
                             className='techs-icon'
                             onMouseEnter={() => setHover('Utilização do figma para desenvolvimento de interfaces.')}
                             onMouseLeave={() => setHover('')}
                         />
-                        <LiaGitAlt 
+                        <LiaGitAlt
                             className='techs-icon'
                             onMouseEnter={() => setHover('Utilização do GIT para versionamento do código e desenvolvimento em equipe.')}
                             onMouseLeave={() => setHover('')}
                         />
                     </div>
-        
+
                 </div>
 
             </div>
